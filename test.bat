1d904d4b32fdf9459d8554d79f1555d943a1e7ca
玉和道 <kenzihashimoto19810122@gmail.com>
2026-07-03 20:54:53 +0900

    NEW_KUMO_FILE

--- test.bat
+++ test.bat
@@ -1,5 +1,6 @@
 @echo off
 
+set app_key=your_key_in_the_here
 set /p ISBN="ISBN‚ð“ü—Í"
-FOR /F "tokens=1" %%i in (lib_my_HOME_to.txt) do curl "https://api.calil.jp/check?appkey={50117474d5268b2c0cebdb7532e39c21}&isbn=%ISBN%&systemid=%%i&format=json&callback=no"
+FOR /F "tokens=1" %%i in (lib_my_HOME_to.txt) do curl "https://api.calil.jp/check?appkey={%app_key%}&isbn=%ISBN%&systemid=%%i&format=json&callback=no"
 start https://calil.jp/book/{%ISBN%}
